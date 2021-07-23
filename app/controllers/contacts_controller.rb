a9ad73c99d7193bf5dc8d82cc2a52acc4e2c8f6c
Raymond <raymond@philippinedev.com>
2021-07-23 14:14:46 +0800

    before action set_contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,10 +1,23 @@
 class ContactsController < ApplicationController
+  before_action :set_contact, only: %i[ show edit update destroy ]
+
   def index
     @contacts = Contact.all
   end
 
   def new
     @contact = Contact.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @contact.update(contact_params)
+      redirect_to contacts_path, notice: "Contact was successfully updated"
+    else
+      render :edit
+    end
   end
 
   def create
@@ -18,6 +31,10 @@ class ContactsController < ApplicationController
   end
 
   private
+
+  def set_contact
+    @contact = Contact.find(params[:id])
+  end
 
   def contact_params
     params.require(:contact).permit(:name, :number, :email)
